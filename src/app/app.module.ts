70e52bc2d557671e4bbd0f36467030b79eed26af
Naveen Puppala <Naveen.Puppala@soti.net>
2023-08-22 13:14:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { OfferRideComponent } from './offer-ride/offer-ride.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    OfferRideComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
